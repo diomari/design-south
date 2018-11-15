11a88323c16460f6a5fb5164856de27e929ee487
Diomari Madulara <diomari.me@gmail.com>
2018-11-15 19:26:13 +0800

    Add speaker modal

---
+++ src/scripts/data.js
@@ -0,0 +1,78 @@
+const data = [
+  {
+    name: `Lei Melendres`,
+    company: 'Side Hustle Inc',
+    place: 'Taguig | Laguna',
+    description: `Lei is the leader of Doodle Art Enthusiasts (DAE), the Philippines' largest doodling community. DAE counts 35,000+ online followers on their Facebook page. A vibrant community, DAE publishes its own magazine, Doodle Arts Magazine, as well as events like Doodle Fest, an annual celebration of creativity where artists submit entries to be curated by DAE for exhibition.
+    Lei forms one-half of the duo behind the design agency Side Hustle Inc, a new venture formed just this year. Lei's works can be found in products and materials by NBA Store, Mazda, Mini Cooper, Samsonite RED, G-Shock PH, Globe, Viber, GAP, BGC, SM MOA, Megaworld, Daniel Wellington, WWF Philippines, ASPACE MNL, Titus Pens, Sharpie, Faber Castell, Stabillo, Caran d’ Ache, Tombow, Dong-A, American Embassy, and National Bookstore.`,
+    id: `lei`,
+    socials: [
+      'http://behance.net/leimelendres',
+      'https://www.instagram.com/leimelendres/'
+    ]
+  },
+  {
+    name: `John Amor`,
+    company: 'Freelance',
+    place: 'Davao | Cebu',
+    description: `A freelance Cebuano artist based in Davao City, John is a freelancer whose works have been published by American comic book publishers like Image Comics, IDW Comics and DC's former webcomic platform ZudaComics.
+    John has worked as a full-time comic creator since 2008. He is behind the art of titles such as Judas: The Last Days, One Week in the Library, and Hank Thatcher Dragon Catcher. His artbook Fictionaut collects selected works and accompanying commentaries from 2017. John also produces creator-owned work like OSCURO, an ongoing webcomic funded through Patreon; as well as the upcoming title Urban Animal, soon via LINE webtoon.
+    John is notable for his work on One Week in the Library, an experimental graphic novella that was published in Image Comics in 2016. His considerable contributions bagged him a nod for Best Short Story in the 2017 Eisner Awards.`,
+    id: `john`,
+    socials: [
+      'https://www.instagram.com/john_amor/',
+      'https://www.facebook.com/johnamor'
+    ]
+  },
+  {
+    name: `Pwek Kwong`,
+    company: 'Bluqube, Inc',
+    place: 'Iloilo',
+    description: `Pwek Kwong is the Chief Executive Officer and Creative Director of Bluqube, Inc in Iloilo City.
+    Bluqube is a creative design firm that caters to the marketing and advertising needs of business establishments worldwide. Bluqube's expertise cover a wide range of services, from advertising campaigns, social media marketing and management, photography, animation, brand films, to website design & development, graphic design, Instagram videos, company AVPs and TVCs.
+    With the tagline "We Create Masterbrands", Bluqube is the creative force behind some memorable brands like Lechon Haus, TEDxUP Visayas and TARA. In 2017, Bluqube also organized Creative Anarchy, the biggest International Conference in design, motion, and tech of the Western Visayas region, thus expanding the possibilities of design in the area.`,
+    id: `pwek`,
+    socials: [
+      'http://www.bluqube.ph',
+      'https://www.instagram.com/bluqube/'
+    ]
+  },
+  {
+    name: `Nikko Bryle Tan`,
+    company: 'Misfit Productions',
+    place: 'Cagayan de Oro',
+    description: `Nikko Bryle Tan is the Creative Director of Misfit Productions, the award-winning studio with branches in Cagayan, Bristol, and Vancouver under Misfit Inc.
+    Misfit Inc., is a design, communication, and business intelligence agency that empowers rebellious clients on every habitable continent. Misfit Productions is its animation and video production team, and the studio behind the award-winning 2017 short film Confection. They handcraft animated stories with partners across the globe.
+    With their expertise in illustration, 2D and 3D animation, the works of Nikko and his team at Misfit Productions graced companies, brands, individuals and NGOs based everywhere from Silicon Valley to rural Laos.`,
+    id: `nikko`,
+    socials: [
+      'https://vimeo.com/misfitprod/',
+      'https://www.instagram.com/misfit_productions/'
+    ]
+  },
+  {
+    name: `Alfven Ato`,
+    company: 'Freelance Artist',
+    place: 'Davao',
+    description: `Alfven took up Multimedia Design and Animation at Holy Child IT Academy. He found employment after his studies at Puppeteer Studios, where he was involved in environment and background design. Here, his love for visual development for games and film grew. After working for Puppeteer Studios, he chose to jump into the world of freelance. As a freelance artist, Alfven works as a concept designer and 2D environment artist for mobile games. Alfven works with clients from all over the world on providing concepts, key art illustrations and environment art.`,
+    id: `alfven`,
+    socials: [
+      'https://alfvenato.artstation.com/'
+    ]
+  },
+  {
+    name: `Aurthur Mercader`,
+    company: 'Puppeteer Studios',
+    place: 'Davao | Makati',
+    description: `Aurthur Mercader graduated with a degree in Digital Illustration and 3D Animation from Ateneo de Naga University, eventually racking up a diploma for the 3D Animation and Visual Effects Program from Vancouver Film School.
+    Since 2013, Aurthur is the Lead Production Artist and Creative Director for Puppeteer Studios, where he specializes in 3D Animation. Puppeteer Studios is a premiere Philippine 3D animation studio that creates some of the most cutting-edge character animation for Filipino feature films, TV series, commercials, video games and more.
+    Aurthur's work with Puppeteer Studios has received widespread recognition. Most notably, he and his team bagged the Manila Bulletin Entertainment Best Animation Award at the 2015 Metro Manila Film Festival for the animated short film "Geo". They also won accolades for Grand Prize: Best Animated Short Film, Best in Category: Animated Short film, Best in Technical Quality, and Best Professional Entry for "Geo" at the Animahenasyon 2015. Their most recent effort, "Unos", brought home the award for Best Animated Film at the 2017 Mindanao Film Festival and Best Technical Entry at Animahenasyon 2017.`,
+    id: `aurthur`,
+    socials: [
+      'https://www.facebook.com/PuppeteerStudios/',
+      'https://aurthurm.artstation.com/'
+    ]
+  }
+];
+
+export default data;
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,108 @@
 import '../styles/index.scss';
+import data from './data';
+
+const static_url = 'public';
+
+const parser = document.createElement('a');
+
+let baseArrow = 0;
+
+// map what icon to use on links
+const iconLinks = {
+    facebook: 'facebook-official',
+    behance: 'behance',
+    instagram: 'instagram',
+    website: 'globe',
+    vimeo: 'vimeo-square'
+};
+
+const detectIcon = (links) => {
+    // remove the name from the url then map to the matching iconLink
+    let r = /^(?:https?:)?(?:\/\/)?([^\/\?]+)/;
+    for(let link of links) {
+        parser.href = link;
+        console.log(parser.hostname);
+    }
+    return '';
+};
+
+// Detect change on vertical scroll then apply rotate style on stamp
+window.onscroll = (ev) => {
+    let theta = window.scrollY / 10 % Math.PI;
+    baseArrow += window.scrollY/ 10 % 4;
+    document.querySelector('.stamp').style.transform = `rotate(${theta}rad)`;
+    document.querySelector('.arrows').style.transform = `translateY(${baseArrow}px)`;
+};
+
+// Generates modal template on each speakers
+function generateModal(speaker){
+    const name = speaker.slice(1,speaker.length);
+    const user = data.find(item => item.id === name.toLowerCase());
+
+    const baseTemplate = `<div class="modal fade" id="${name}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+      <div class="modal-content">
+        <div class="modal-body">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+            <div class="row">
+                <div class="col-lg-6 modal-left modal-content">
+                        <div class="speaker-header row">
+                            <div class="col-4 speaker-avatar">
+                                <img src="${static_url}/images/${user.id}.jpg" class="img-fluid rounded-circle" />
+                            </div>
+                            <div class="col-8 speaker-info">
+                                <h2 class="speaker-name">${user.name}</h2>
+                                <h3 class="speaker-company">${user.company}</h3>
+                                <h3 class="speaker-place">${user.place}</h3>
+                            </div>
+                        </div>
+                    <p class="speaker-description"> ${user.description} </p>
+                    <ul class="speaker-medias list-inline">
+                        <li class="list-inline-item"> <a class="soc-facebook" title="Facebook"></a></li>
+                        <li class="list-inline-item"> <a class="fa fa-facebook"></a> tw</li>
+                        <li class="list-inline-item"> <a class="fa fa-facebook"></a> ig</li>
+                    </ul>
+                </div>
+                <div class="col-lg-6 modal-right modal-portfolio">
+                    <div class="row no-gutters">
+                        <div class="col-12">
+                        <img src="${static_url}/images/${user.id}/1.jpg" class="img-fluid" />
+                        </div>
+                    </div>
+                    <div class="row no-gutters">
+                        <div class="col-6">
+                        <img src="${static_url}/images/${user.id}/2.jpg" class="img-fluid" />
+                        </div>
+                        <div class="col-6">
+                        <img src="${static_url}/images/${user.id}/3.jpg" class="img-fluid" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+          
+        </div>
+       
+      </div>
+    </div>
+  </div>`;
+    detectIcon(user.socials);
+    let e = document.createElement('div');
+    e.innerHTML = baseTemplate;
+    let speakerModal = document.querySelector('#speaker-modal');
+    speakerModal.appendChild(e);
+}
+
+// Trigger when DOM is loaded
+document.addEventListener("DOMContentLoaded",function(){
+    const speakers = document.querySelectorAll('.diamonds-item');
+    for (let speaker of speakers) {
+        if (speaker.dataset.target) {
+            generateModal(speaker.dataset.target);
+        }
+        speaker.addEventListener('click', (e) => {
+            e.preventDefault();
+        });
+    }
+});
